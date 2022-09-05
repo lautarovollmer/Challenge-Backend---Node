fb8e223d46e914e912d89dfb74bfd0106ed79673
lautarovollmer <lautarovollmer0@gmail.com>
2022-09-05 16:25:29 -0300

    index

--- src/Controller/index.ts
+++ src/Controller/index.ts
@@ -1,5 +1,5 @@
 import express, { Router } from "express";
-import { createCharacter, getPersonajes, editCharacter } from "../Services/characters";
+import { createCharacter, getPersonajes, editCharacter, deleteCharacter } from "../Services/characters";
 import { loginUser, registerUser } from "../Services/user";
 import { verifyToken } from "../Services/verifyToken";
 
@@ -13,5 +13,6 @@ router.post("/login", loginUser);
 router.get("/characters", verifyToken, getPersonajes);
 router.post("/characters", verifyToken, createCharacter);
 router.put("/characters", verifyToken, editCharacter);
+router.delete("/characters", verifyToken, deleteCharacter);
 
 export default router;
